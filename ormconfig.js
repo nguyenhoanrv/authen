602a7fd673cb537a052b36700781985dc4da0e6a
Nguyen Hoan <hoan.nv.bk63@gmail.com>
2021-07-06 14:24:38 +0700

    fix environment

--- ormconfig.js
+++ ormconfig.js
@@ -1,24 +1,22 @@
-// const connection = process.env.DB_CONNECTION;
-// const host = process.env.DB_HOST;
-// const port = process.env.DB_PORT;
-// const username = process.env.DB_USERNAME;
-// const password = process.env.DB_PASSWORD;
-// const database = process.env.DB_DATABASE;
-// const environment = process.env.NODE_ENV;
+const connection = process.env.DB_CONNECTION;
+const host = process.env.DB_HOST;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_DATABASE;
+const environment = process.env.NODE_ENV;
 
 module.exports = {
-  // type: connection,
-  // host: host,
-  // port: port,
-  // username: username,
-  // password: password,
-  // database: database,
-  type: 'postgres',
-  host: 'postgres',
-  // port: '5432',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'test1',
+  type: connection,
+  host: host,
+  username: username,
+  password: password,
+  database: database,
+  // type: 'postgres',
+  // host: 'postgres',
+  // // port: '5432',
+  // username: 'postgres',
+  // password: 'postgres',
+  // database: 'test1',
 
   entities: ['dist/infrastructure/database/mapper/*.js'],
 
